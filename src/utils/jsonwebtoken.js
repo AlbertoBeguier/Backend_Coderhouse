e3c6e50300e4ba5b70883f4ee45209c9031ba104
Alberto Beguier <aabeguier@hotmail.com>
2024-09-21 09:10:13 -0300

    primer entrega backend II ok

--- src/utils/jsonwebtoken.js
+++ src/utils/jsonwebtoken.js
@@ -4,7 +4,6 @@ const private_key = "palabrasecretaparatoken";
 
 const generateToken = (user) => {
   const token = jwt.sign(user, private_key, { expiresIn: "24h" });
-  //Le puedo colocar una fecha de expiracion.
   return token;
 };
 
