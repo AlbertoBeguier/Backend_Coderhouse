4e6fd1e83af74f0ef94aae93dcccccee5a40c8a4
Alberto Beguier <aabeguier@hotmail.com>
2024-10-10 11:48:50 -0300

    variable de entorno para conectarse a la base de datos

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Cargar variables de entorno
+dotenv.config();
 
 mongoose
-  .connect(
-    "mongodb+srv://aabeguier:5279167134@clustercoder.kzn29.mongodb.net/ProyectoCoder?retryWrites=true&w=majority&appName=ClusterCoder",
-    {}
-  )
+  .connect(process.env.MONGODB_URI, {})
   .then(() => console.log("Conectado a MongoDB"))
   .catch((err) => console.error("Error al conectar a MongoDB:", err));
